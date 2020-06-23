eb0ab4de42aa9bd35e650206e722521d55ee4810
HallexCosta <hallex.costa@hotmail.com>
2020-06-23 18:53:52 -0300

    Restructuring modules.export

--- pages/vai-jogar-o-que/itemsCards.js
+++ pages/vai-jogar-o-que/itemsCards.js
@@ -2,148 +2,144 @@ window.addEventListener('load', () => {
   const rootDirectory = '../..'
   const prefixURL = `${rootDirectory}/assets`
 
-  function path(...directories) {
-    const fullDirectory = directories.join('/')
-    return fullDirectory
-  }
-
-  const dataCards = [
-    {
-      title: 'Battlefield 1',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'battlefield-i'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'GTA V',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'gta-v'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'The Witcher 3 - Wild Hunt',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'the-witcher-3-wild-hunt'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'League of Legends',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'league-of-legends'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Dota 2',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'dota-2'),
-        extension: 'jpg'  
-      }
-    },
-    {
-      title: 'Minecraft',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'minecraft'),  
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Counter-Strike: Global Offensive',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'counter-strike-global-offensive'),
-        extension: 'jpg'  
-      }
-    },
-    {
-      title: 'Rainbow Six Siege',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'rainbow-six-siege'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Overwatch',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'overwatch'),
-        extension: 'jpg'  
-      }
-    },
-    {
-      title: 'Tom Clancy\'s - The division',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'tom-clancy-s-the-division'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Assasins Creed - Origins',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'assasins-creed-origins'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Fortnite',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'fortnite'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Fifa 2018',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'fifa-2018'),
-        extension: 'jpg'
-      }
-    },
-    {
-      title: 'Roblox',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'roblox'),
-        extension: 'png'
-      }
-    },
-    {
-      title: 'Paladins',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'paladins'),
-        extension: 'png'
-      }
-    },
-    {
-      title: 'Apex Legends',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'apex-legends'),
-        extension: 'png'
-      }
-    },
-    {
-      title: 'Call of Duty Black Ops 4',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'call-of-duty-black-ops-4'),
-        extension: 'png'
-      }
-    },
-    {
-      title: 'Battlefield V',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'battlefield-v'),
-        extension: 'png'
-      }
-    },
-    {
-      title: 'Metro Exodus',
-      image: {
-        url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'metro-exodus'),
-        extension: 'png'
-      }
-    }
-  ]
+  const { Helpers } = modules.export
+  const { path } = Helpers()
 
   modules.export = {
-    dataCards
+    dataCards: [
+      {
+        title: 'Battlefield 1',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'battlefield-i'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'GTA V',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'gta-v'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'The Witcher 3 - Wild Hunt',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'the-witcher-3-wild-hunt'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'League of Legends',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'league-of-legends'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Dota 2',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'dota-2'),
+          extension: 'jpg'  
+        }
+      },
+      {
+        title: 'Minecraft',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'minecraft'),  
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Counter-Strike: Global Offensive',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'counter-strike-global-offensive'),
+          extension: 'jpg'  
+        }
+      },
+      {
+        title: 'Rainbow Six Siege',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'rainbow-six-siege'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Overwatch',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'overwatch'),
+          extension: 'jpg'  
+        }
+      },
+      {
+        title: 'Tom Clancy\'s - The division',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'tom-clancy-s-the-division'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Assasins Creed - Origins',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'assasins-creed-origins'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Fortnite',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'fortnite'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Fifa 2018',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'fifa-2018'),
+          extension: 'jpg'
+        }
+      },
+      {
+        title: 'Roblox',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'roblox'),
+          extension: 'png'
+        }
+      },
+      {
+        title: 'Paladins',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'paladins'),
+          extension: 'png'
+        }
+      },
+      {
+        title: 'Apex Legends',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'apex-legends'),
+          extension: 'png'
+        }
+      },
+      {
+        title: 'Call of Duty Black Ops 4',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'call-of-duty-black-ops-4'),
+          extension: 'png'
+        }
+      },
+      {
+        title: 'Battlefield V',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'battlefield-v'),
+          extension: 'png'
+        }
+      },
+      {
+        title: 'Metro Exodus',
+        image: {
+          url: path(prefixURL, 'pages',  'vai-jogar-o-que', 'games', 'metro-exodus'),
+          extension: 'png'
+        }
+      }
+    ]
   }
 })
