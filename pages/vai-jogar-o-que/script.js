416339c2b611fa0c401b1cd71491df70b41fce5f
HallexCosta <hallex.costa@hotmail.com>
2020-06-23 17:32:48 -0300

    Finish list games of page vai-jogar-o-que

--- pages/vai-jogar-o-que/script.js
+++ pages/vai-jogar-o-que/script.js
@@ -1,6 +1,81 @@
 window.addEventListener('load', () => {
-  const titles = {}
-  const imgs = {}
+  const { dataCards } = modules.export
 
-  function cloneCard() {}
+  function createListGames(selector) {
+    const cards = createCardsGroup(selector).get()
+
+    dataCards.map(({ title, image }) => {
+      const card = createCard()
+      const img = createElementImage().setURL(`${image.url}.${image.extension}`)
+      const legend = createElementLegend().setContent(title)
+
+      card.add(img)
+      card.add(legend)
+      cards.appendChild(card.get())
+    })
+
+    return {
+      getCards: () => cards
+    }
+  }
+
+  function createCardsGroup(selector) {
+    const cards = document.querySelector(selector)
+    //REMOVER ESSA FUNÇÃO
+    const add = card => {
+      cards.appendChild(card)
+    }
+
+    return {
+      add,
+      get: () => cards
+    }
+  }
+
+
+  function createElementLegend() {
+    const legend = document.createElement('legend')
+
+    const setContent = (content) => {
+      const title = document.createTextNode(content)
+      legend.appendChild(title)
+      return legend
+    }
+
+    return {
+      setContent
+    }
+  }
+
+  function createElementImage() {
+    const image = document.createElement('img')
+
+    const setURL = url => {
+      image.setAttribute('src', url)
+      return image
+    }
+
+    return {
+      setURL
+    }
+  }
+
+  function createCard() { 
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const add = element => {
+      return card.appendChild(element)
+    } 
+
+    return {
+      add,
+      get: () => card
+    }
+  }
+
+  modules.export = {
+    createListGames
+  }
 })
+
