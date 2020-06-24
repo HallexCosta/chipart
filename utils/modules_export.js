5c773c05194847c490efd4abeb53c841c7ec74e1
HallexCosta <hallex.costa@hotmail.com>
2020-06-24 00:37:48 -0300

    Refactoring modules_export.js

--- utils/modules_export.js
+++ utils/modules_export.js
@@ -1,1 +1,16 @@
-window.modules = { export: {} }
+window.modules = {
+  constructor(){
+    this.exports = {}
+  },
+  get export() { 
+    return this.exports
+  },
+  set export(values){
+    this.exports = {
+      ...this.exports,
+      ...values
+    } 
+  }
+}
+
+
