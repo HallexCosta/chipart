abb830207b7c8f82c11c81aa46a9149a1bf9b31b
HallexCosta <hallex.costa@hotmail.com>
2020-06-24 16:59:26 -0300

    Initializing page monte-seu-pc-gamer

---
+++ pages/monte-seu-pc-gamer/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  console.log('Load my script')
+  const { rootPath } = module.exports
+
+  const pieces = [
+    {
+      title: 'PROCESSADOR',
+      svg: path()
+    }
+  ]
+
+})
