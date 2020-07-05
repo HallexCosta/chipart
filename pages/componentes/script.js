70b7087d1cad7a9ffd25d0813d52ed8014ed98b6
HallexCosta <hallex.costa@hotmail.com>
2020-07-05 13:57:39 -0300

    Ading page componentes

---
+++ pages/componentes/script.js
@@ -0,0 +1,90 @@
+window.addEventListener('load', () => {
+  const listProducts = createListProducts()
+  listProducts.changeIconAndExpandButtonCategorias()
+  listProducts.addClassActivedOnLinkClicked()
+  listProducts.cloneCards(9).run()
+
+  function createListProducts() {
+    const buttonCategorias = document.querySelector('.container aside .list-product .vertical .title')
+    const verticalItems = document.querySelector('.container aside .list-product .vertical .items')
+
+    changeIconAndExpandButtonCategorias = () => {
+      const icons = {
+        plus: 'fi-plus',
+        minus: 'fi-minus'
+      }
+      const icon = document.querySelector('.container aside .list-product .vertical li i')
+      const { plus, minus } = icons
+      buttonCategorias.addEventListener('click', () => {
+        //console.log('Clicked')
+        changeIcons()
+        expandCategories()
+      })
+
+      changeIcons = () => {
+        const currentClass = icon.getAttribute('class')
+        //console.log(`Current class: ${currentClass}`)
+        if (currentClass == plus) {
+          //console.log(`Change ${plus} for ${minus}`)
+          icon.classList.remove(plus)
+          icon.classList.add(minus)
+        } else {
+          //console.log(`Change ${minus} for ${plus}`)
+          icon.classList.remove(minus)
+          icon.classList.add(plus)
+        }
+      }
+        
+      expandCategories = () => {
+        const currentClass = icon.getAttribute('class')
+          
+        if (currentClass == plus) {
+          verticalItems.style.maxHeight = '0'
+        } else {
+          verticalItems.style.maxHeight = '300px' 
+        }
+      }
+    }
+
+    addClassActivedOnLinkClicked = () => {
+      const links = document.querySelectorAll('.container aside .list-product .vertical .items li a')
+      
+      //console.log('Call addClassActivedOnLinkClicked')
+
+      links.forEach(link => {
+        link.addEventListener(('click'), () => {
+          links.forEach(link => {
+            link.style.color = '#000'
+            //console.log('Load black color in', link)
+          })
+          link.style.color = '#6fc81e'
+        })
+      })
+    }
+
+    cloneCards = (recursive) => {
+      let controlRecursive = 0
+      const cards = document.querySelector('.cards')
+      const card = document.querySelector('.card')
+      
+      const run = () => {
+        if (controlRecursive !== Number(recursive)) {
+          cards.innerHTML += '<div class="card">' + card.innerHTML + '</div>'          
+          //console.log(controlRecursive)
+          controlRecursive += 1
+          run()
+        }
+      }
+
+      return {
+        run
+      }
+    }
+
+    return {
+      addClassActivedOnLinkClicked,
+      changeIconAndExpandButtonCategorias,
+      cloneCards
+    }
+  }
+})
